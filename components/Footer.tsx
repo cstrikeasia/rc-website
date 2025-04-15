edfef7b783a4558d0cddcd223a8c466bd8b42a51
Miyashooooo <cstrike.asia@gmail.com>
2025-04-15 23:18:03 +0800

    fix: footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -9,27 +9,41 @@ export const Footer = () => {
     {
       label: "常見問題",
       href: "/faq",
+      target: "",
     },
     {
       label: "使用條款協議",
       href: "/agreement",
+      target: "",
     },
     {
       label: "RC語音平台規範",
       href: "/specification",
+      target: "",
     },
-    { label: "聯絡我們", href: "/contactus" },
+    { label: "聯絡我們", href: "/contactus", target: "" },
     {
       label: "問題回報",
-      href: "https://discord.gg/adCWzv6wwS",
+      href: "https://github.com/NerdyHomeReOpen/RiceCall/issues",
+      target: "_blank",
+    },
+    {
+      label: "伺服器狀態",
+      href: "https://status.ricecall.com.tw/",
+      target: "_blank",
     },
   ];
   return (
     <div className={footer["footer"]}>
       <div className={footer["wrapper"]}>
         <div>
-          {footerLinks.map(({ label, href }) => (
-            <a key={label} href={href}>
+          {footerLinks.map(({ label, href, target }) => (
+            <a
+              key={label}
+              href={href}
+              target={target}
+              rel={target === "_blank" ? "noreferrer" : undefined}
+            >
               {label}
             </a>
           ))}
