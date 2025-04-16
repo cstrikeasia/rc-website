a4f0b7205c8ff22f043610296925c58bf8d6ad50
Miyashooooo <cstrike.asia@gmail.com>
2025-04-16 13:09:07 +0800

    feat: ico

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,9 @@
 // CSS
 import "@/styles/global.css";
 
+// IMAGE
+import ico from "@/images/favicon.ico";
+
 export const metadata = {
   title: "RiceCall - 有趣的語音聊天平台",
   description: "RiceCall - 有趣的語音聊天平台",
@@ -12,6 +15,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="zh-Hant">
+      <head>
+        <link rel="icon" href={ico.src} sizes="any" />
+      </head>
       <body>{children}</body>
     </html>
   );
